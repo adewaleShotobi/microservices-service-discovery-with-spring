95995188fdf408e8c8edba5ff4d0464e2c0b020b
Adewale <sholekbiz@gmail.com>
2020-01-20 22:13:12 +0100

    Added Movie Catalog API

---
+++ movie-catalog-service/src/main/java/com/example/moviecatalogservice/resources/MovieCatalogResource.java
@@ -0,0 +1,23 @@
+package com.example.moviecatalogservice.resources;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.moviecatalogservice.models.CatalogItem;
+
+@RestController
+@RequestMapping("/catalog")
+public class MovieCatalogResource {
+	
+	@RequestMapping("/{userId}")
+	public List<CatalogItem> getCatalog(@PathVariable String userId){
+		return Collections.singletonList(
+				new CatalogItem("SEAL Team","test",4)
+				);
+	}
+
+}
